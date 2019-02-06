826b8baff2728f485843c5b49f43023686c6e512
pattywitwhy <pattyxhwang@gmail.com>
2019-02-06 17:16:26 -0600

    client recipe views

---
+++ app/controllers/client/recipes_controller.rb
@@ -0,0 +1,13 @@
+class Client::RecipesController < ApplicationController
+  def index
+    response = HTTP.get("http://localhost:3000/api/recipes")
+    @recipes = response.parse
+    render 'index.html.erb'
+  end
+
+  def show
+    response = HTTP.get("http://localhost:3000/api/recipes/#{params[:id]}")
+    @recipe = response.parse
+    render 'show.html.erb'
+  end
+end
